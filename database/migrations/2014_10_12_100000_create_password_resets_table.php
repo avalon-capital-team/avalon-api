f74829c029de964f265a9022ac75872719ddf3d9
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-02-15 00:52:14 -0300

    update migrete

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -13,6 +14,7 @@ return new class () extends Migration {
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
