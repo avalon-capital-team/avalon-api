a95ac2283955d1f16a40b0ff2d4f8703db574483
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-02-17 01:13:31 -0300

    update migrate notification

--- database/migrations/2022_09_29_135543_create_notifications_table.php
+++ database/migrations/2022_09_29_135543_create_notifications_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('primary_id');
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
