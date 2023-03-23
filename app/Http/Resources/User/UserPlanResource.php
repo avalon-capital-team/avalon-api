4e85540f7fe03e4fc0976c051ad167cc15a60cd5
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-03-23 17:08:15 -0300

    update

--- app/Http/Resources/User/UserPlanResource.php
+++ app/Http/Resources/User/UserPlanResource.php
@@ -51,7 +51,7 @@ class UserPlanResource
             ]);
         $order = (new Order())->createOrder($user, $validated);
         $credit = (new CreditResource())->create($user, $validated['coin_id'], $validated['plan_id'], $type_id, $status_id, $validated['amount'], $description, $order->id);
-        $plan = (new PlanResource())->create($user, $user_plan->id, $validated['plan_id'], $validated['coin_id'], $validated['amount'], $validated['income'], $validated['acting'], $validated['payment_voucher_url']);
+        // $plan = (new PlanResource())->create($user, $user_plan->id, $validated['plan_id'], $validated['coin_id'], $validated['amount'], $validated['income'], $validated['acting'], $validated['payment_voucher_url']);
 
         if (!$user_plan && !$order && !$credit  && !$plan) {
             throw new \Exception('Não foi possível gerar a orden. Tente novamente mais tarde!');
