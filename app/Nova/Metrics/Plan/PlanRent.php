2d2ceb90baca4aa5cc86ea178375f43baa6dd29b
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-04-13 13:33:16 -0300

    Fix

--- app/Nova/Metrics/Plan/PlanRent.php
+++ app/Nova/Metrics/Plan/PlanRent.php
@@ -44,7 +44,7 @@ class PlanRent extends Value
      */
     public function calculate(NovaRequest $request)
     {
-        return $this->sum($request, Plan::where('coin_id', $this->coin_id), 'amount')->format('0.00');
+        return $this->sum($request, Plan::where('coin_id', $this->coin_id), 'income')->format('0.00');
     }
 
     /**
