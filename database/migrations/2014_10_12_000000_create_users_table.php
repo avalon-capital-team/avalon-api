be0e82796bf235a5e81d4e99d9288deebc636931
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-04-27 17:00:20 -0300

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class() extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->enum('type', ['user', 'mananger', 'advisor', 'admin'])->default('user');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('document_type', array('CPF', 'CNPJ'))->nullable();
