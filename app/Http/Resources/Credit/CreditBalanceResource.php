4e35541018f1198f4a7d366132fe28ba24e6b4e1
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-04-28 01:34:34 -0300

    fix

--- app/Http/Resources/Credit/CreditBalanceResource.php
+++ app/Http/Resources/Credit/CreditBalanceResource.php
@@ -56,9 +56,9 @@ class CreditBalanceResource
 
         $data = [
             'balance_enable' => $creditBalance->balance_placed,
-            'balance_pending' => $creditBalance->balance_placed,
+            'balance_pending' => $creditBalance->balance_pending,
             'balance_placed' => $creditBalance->balance_placed,
-            'balance_rendeem' => $creditBalance->withdrawal,
+            'balance_rendeem' => $creditBalance->balance_placed,
             'balance_income' => $creditBalance->income,
             'pie_chart' => [
                 'placed' => $balance_placed,
