3e80e22faec7c4d9ab9bd6b8fbadef936125bf15
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-07-07 19:40:37 -0300

    update

--- app/Http/Resources/System/Rules/LimitOfUserResource.php
+++ app/Http/Resources/System/Rules/LimitOfUserResource.php
@@ -10,33 +10,32 @@ use App\Models\Withdrawal\WithdrawalFiat;
 
 class LimitOfUserResource
 {
-    /**
-     * @param  \App\Models\User $user;
-     * @return array
-     */
-    public function checkLimits(User $user)
-{
+  /**
+   * @param  \App\Models\User $user;
+   * @return array
+   */
+  public function checkLimits(User $user)
+  {
     $limitsSystem = LimitOfUser::first();
 
     if ($user->compliance) {
-        # User validated
-        if ($user->userPlan->coin_id == 1) {
-            $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalFiat::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
-        }
-        $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalCrypto::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
+      # User validated
+      if ($user->userPlan->coin_id == 1) {
+        $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalFiat::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
+      }
+      $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalCrypto::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
     } else {
-        # User not validated
-        if ($user->userPlan->coin_id == 1) {
-            $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalFiat::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
-        }
-        $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_not_validated - WithdrawalCrypto::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
+      # User not validated
+      if ($user->userPlan->coin_id == 1) {
+        $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_validated - WithdrawalFiat::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
+      }
+      $data['withdrawal'] = $limitsSystem->withdrawal_fiat_user_not_validated - WithdrawalCrypto::where('status_id', '!=', 1)->where('user_id', $user->id)->where('created_at', 'LIKE', date('Y-m') . '%')->sum('amount');
     }
 
     if ($data['withdrawal'] < 0) {
-        $data['withdrawal'] = 0;
+      $data['withdrawal'] = 0;
     }
 
     return $data;
-}
-    }
+  }
 }
