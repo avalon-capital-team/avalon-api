2e600374d10b7baffd9d3154521b00545e9aed89
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-07-11 18:27:19 -0300

    update

--- app/Http/Controllers/Api/Wallet/WithdrawalController.php
+++ app/Http/Controllers/Api/Wallet/WithdrawalController.php
@@ -13,68 +13,73 @@ use Illuminate\Http\Request;
 
 class WithdrawalController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:sanctum');
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth:sanctum');
+  }
+
+  /**
+   * @param  \Illuminate\Http\Request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function withdrawlPlan(Request $request)
+  {
+    try {
+      $user = User::find($request->user_id);
+      if ($user) {
+        (new PlanResource())->withdrawalPlan($user, $request->amount);
+        (new WithdrawalFiatResource())->createWithdrawal($user, $request->coin_id, $request->type, $request->amount);
+      } else {
+        (new PlanResource())->withdrawalPlan(auth()->user(), $request->amount);
+        (new WithdrawalFiatResource())->createWithdrawal(auth()->user(), $request->coin_id, $request->type, $request->amount);
+      }
+      return response()->json([
+        'status' => true,
+        'message' => 'A solicitação de saque foi realizada com sucesso.'
+      ], 200);
+    } catch (\Exception $e) {
+      return response()->json([
+        'status'  => false,
+        'message' => $e->getMessage()
+      ], $e->getCode() ?? 400);
     }
+  }
 
-    /**
-     * @param  \Illuminate\Http\Request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function withdrawlPlan(Request $request)
-    {
-        try {
-          $user = User::find($request->user_id);
-            (new PlanResource())->withdrawalPlan($user, $request->amount);
-            (new WithdrawalFiatResource())->createWithdrawal($user, $request->coin_id, $request->type, $request->amount);
-            return response()->json([
-                'status' => true,
-                'message' => 'A solicitação de saque foi realizada com sucesso.'
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status'  => false,
-                'message' => $e->getMessage()
-            ], $e->getCode() ?? 400);
-        }
+  /**
+   * @param  \Illuminate\Http\Request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function withdrawl(WithdrawalFiatRequest $request)
+  {
+    return response()->json([
+      'status' => true,
+      'message' => 'Em manutenção, em breve disponível.'
+    ], 200);
+    try {
+      $validated = $request->validated();
+
+      (new PlanResource())->withdrawalPlan(auth()->user(), $validated['amount']);
+
+      if ($validated['coin_id'] == '1') {
+        (new WithdrawalFiatResource())->createWithdrawal(auth()->user(), $validated['coin_id'], $validated['type'], $validated['amount']);
+      } else {
+        (new WithdrawalCryptoResource())->createWithdrawalCrypto(auth()->user(), $validated['coin_id'], $validated['type'], $validated['amount']);
+      }
+
+      return response()->json([
+        'status' => true,
+        'message' => 'A solicitação de saque foi realizada com sucesso.'
+      ], 200);
+    } catch (\Exception $e) {
+      return response()->json([
+        'status'  => false,
+        'message' => $e->getMessage()
+      ], $e->getCode() ?? 400);
     }
-
-    /**
-     * @param  \Illuminate\Http\Request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function withdrawl(WithdrawalFiatRequest $request)
-    {
-        return response()->json([
-            'status' => true,
-            'message' => 'Em manutenção, em breve disponível.'
-        ], 200);
-        try {
-            $validated = $request->validated();
-
-            (new PlanResource())->withdrawalPlan(auth()->user(), $validated['amount']);
-
-            if ($validated['coin_id'] == '1') {
-                (new WithdrawalFiatResource())->createWithdrawal(auth()->user(), $validated['coin_id'], $validated['type'], $validated['amount']);
-            } else {
-                (new WithdrawalCryptoResource())->createWithdrawalCrypto(auth()->user(), $validated['coin_id'], $validated['type'], $validated['amount']);
-            }
-
-            return response()->json([
-                'status' => true,
-                'message' => 'A solicitação de saque foi realizada com sucesso.'
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status'  => false,
-                'message' => $e->getMessage()
-            ], $e->getCode() ?? 400);
-        }
-    }
+  }
 }
