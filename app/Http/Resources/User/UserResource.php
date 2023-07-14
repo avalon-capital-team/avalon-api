9cab9ba7586eb5351cbf90484ba4a15c2d3a8bcd
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-07-14 16:38:24 -0300

    update

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -92,9 +92,13 @@ class UserResource
         $query->select('id', 'name', 'symbol', 'price_brl', 'price_usd');
       }, 'plan' => function ($query) {
         $query->select('user_id', 'token', 'amount', 'income', 'acting', 'activated_at', 'payment_voucher_url', 'withdrawal_report');
-      }
+      }, 'creditBalance' => function ($query) {
+        $query->select('user_id', 'coin_id', 'balance_enable', 'balance_placed', 'income');
+      }, 'credits' => function ($query) {
+        $query->select('user_id', 'uuid', 'amount', 'base_amount', 'description', 'type_id', 'created_at');
+      },
     ])
-      ->whereHas('userPlan', function ($query) {
+      ->whereHas('plan', function ($query) {
         $query->where('acting', 1);
       })->get();
 
