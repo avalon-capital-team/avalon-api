3feedf0814b50f8fcd39eda38226759dd0a306c2
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-07-14 17:13:39 -0300

    update

--- app/Models/User/UserPlan.php
+++ app/Models/User/UserPlan.php
@@ -20,7 +20,7 @@ class UserPlan extends Model
    * @var string
    */
   protected $table = 'users_plan';
-  // protected $appends = ['total', 'total_month'];
+  protected $appends = ['total', 'total_month'];
 
   /**
    * The attributes that are mass assignable.
