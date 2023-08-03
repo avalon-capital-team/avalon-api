f9d84ffd1d925436dc0cd63a10b10b6426f65e10
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-08-03 13:18:59 -0300

    update

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -128,7 +128,9 @@ class UserResource
         $query->select('user_id', 'cep', 'street', 'neighborhood', 'city', 'state', 'number', 'complement');
       }, 'sponsor' => function ($query) {
         $query->select('id', 'name', 'username', 'email', 'phone', 'type');
-      },'clients', 'userPlan' => function ($query) {
+      },'clients' => function ($query) {
+        $query->select('id', 'name', 'username', 'email', 'sponsor_id', 'phone', 'type');
+      }, 'userPlan' => function ($query) {
         $query->select('plan_id', 'coin_id', 'user_id', 'amount', 'income', 'acting', 'activated_at', 'payment_voucher_url', 'withdrawal_report');
       }, 'userPlan.dataPlan' => function ($query) {
         $query->select('id', 'name', 'rescue', 'porcent');
