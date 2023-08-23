d4cdc9decf3737731e0fc9027a218cf7cb2a6af9
Dougals Camargo Cesario <douglas.camargo.ltda@gmail.com>
2023-08-23 14:02:38 -0300

    update

--- app/Models/User/UserPlan.php
+++ app/Models/User/UserPlan.php
@@ -41,13 +41,6 @@ class UserPlan extends Model
   {
     if ($this->acting == 1) {
       return $this->calculePercent($this);
-      $plans = Plan::where('user_id', $this->user_id)->where('acting', 1)->get();
-      $totalPercentValue = 0;
-      foreach($plans as $plan){
-        $value = $this->calculePercent($plan);
-        $totalPercentValue += $value;
-      }
-      return $totalPercentValue;
     } else {
       return 0;
     }
@@ -145,13 +138,11 @@ class UserPlan extends Model
     } else {
       $percentPeriodo = $days * $percent;
     }
-
-    // if ($plan->amount === $balance->balance_placed) {
+    if ($plan->amount === $balance->balance_placed) {
       $amount = ($plan->withdrawal_report == 0) ? $plan->amount : $plan->amount + $plan->income;
-    // } else {
-    //   $amount = ($plan->withdrawal_report == 0) ? $balance->balance_placed : $plan->amount + $plan->income;
-    // }
-
+    } else {
+      $amount = ($plan->withdrawal_report == 0) ? $balance->balance_placed : $plan->amount + $plan->income;
+    }
     $value = ($percentPeriodo / 100) * $amount;
 
     return $value;
